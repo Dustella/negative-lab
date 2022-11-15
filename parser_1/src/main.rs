12ee9e7bfa8e91d314cc4476e55ebee139a8608c
特菈 Dustella <fdnoaivj@outlook.com>
2022-11-15 12:01:39 +0000

    🔥 sync

--- parser_1/src/main.rs
+++ parser_1/src/main.rs
@@ -1,5 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    let mut raw = String::from("");
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut raw).unwrap();
+    let mut inp = raw.chars().rev().collect::<String>();
+
+    let mut stack = vec![table::State::E];
+
+    while !stack.is_empty() && !inp.is_empty() {
+        let i = inp.chars().last().unwrap();
+        let this = stack.last().unwrap();
+
+        let res = trans(this, i);
+        dbg!(&stack);
+        if let Ok(mut ok) = res {
+            dbg!(&ok);
+            ok.reverse();
+            for i in ok {
+                stack.push(i);
+            }
+        } else {
+            println!("Error");
+            return;
+        }
+        while let table::State::Terminal(t) = stack.last().unwrap() {
+            if *t == i {
+                inp.pop();
+                stack.pop();
+                continue;
+            } else {
+                println!("error {} {}", t, i);
+            }
+        }
+    }
 }
 mod trans;
 use trans::*;
--- parser_1/src/trans.rs
+++ parser_1/src/trans.rs
@@ -1,6 +1,6 @@
 use crate::table::State;
 
-pub fn trans(state: State, cha: char) -> Result<Vec<State>, String> {
+pub fn trans(state: &State, cha: char) -> Result<Vec<State>, String> {
     match state {
         State::E => match cha {
             '(' => Ok(vec![State::T, State::Ee]),
@@ -18,7 +18,13 @@ pub fn trans(state: State, cha: char) -> Result<Vec<State>, String> {
             _ => Err("".to_string()),
         },
         State::Ee => match cha {
-            '+' => Ok(vec![]),
+            '+' => Ok(vec![State::Terminal('+'), State::T, State::Ee]),
+            _ => Err("".to_string()),
         },
+        State::Tt => match cha {
+            '*' => Ok(vec![State::Terminal('*'), State::F, State::Tt]),
+            _ => Err("".to_string()),
+        },
+        State::Terminal(_) => Err("expect terminal".to_string()),
     }
 }
